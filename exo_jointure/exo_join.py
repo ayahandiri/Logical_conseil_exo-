0915f615f4acfb7f2e325de4365cafcb47988a03
Aya Handiri <handiri2005@gmail.com>
2025-01-21 11:27:39 +0100

    rendu

---
+++ exo_jointure/exo_join.py
@@ -0,0 +1,120 @@
+import pandas as pd 
+import numpy as np 
+
+
+data1 = pd.DataFrame({
+    'student_id': ['S1', 'S2', 'S3', 'S4', 'S5'],
+    'name': ['Danniella Fenton', 'Ryder Storey', 'Bryce Jensen', 'Ed Bernal', 'Kwame Morin'],
+    'marks': [200, 210, 190, 222, 199]
+})
+
+data2 = pd.DataFrame({
+    'student_id': ['S4', 'S5', 'S6', 'S7', 'S8'],
+    'name': ['Scarlette Fisher', 'Carla Williamson', 'Dante Morse', 'Kaiser William', 'Madeeha Preston'],
+    'marks': [201, 200, 198, 219, 201]
+})
+
+
+result = pd.concat([data1, data2], ignore_index=True)
+print(result)
+
+
+
+student_data1 = pd.DataFrame({
+    'student_id': ['S1', 'S2', 'S3', 'S4', 'S5'],
+    'name': ['Danniella Fenton', 'Ryder Storey', 'Bryce Jensen', 'Ed Bernal', 'Kwame Morin'],
+    'marks': [200, 210, 190, 222, 199]
+})
+
+student_data2 = pd.DataFrame({
+    'student_id': ['S4', 'S5', 'S6', 'S7', 'S8'],
+    'name': ['Scarlette Fisher', 'Carla Williamson', 'Dante Morse', 'Kaiser William', 'Madeeha Preston'],
+    'marks': [201, 200, 198, 219, 201]
+})
+
+
+result1 = pd.concat([student_data1, student_data2], axis=1)
+print(result1)
+
+
+
+students_data = pd.DataFrame({
+    'student_id': ['S1', 'S2', 'S3', 'S4', 'S5'],
+    'name': ['Danniella Fenton', 'Ryder Storey', 'Bryce Jensen', 'Ed Bernal', 'Kwame Morin'],
+    'marks': [200, 210, 190, 222, 199]
+})
+
+new_row = pd.DataFrame({
+    'student_id': ['S6'],
+    'name': ['Scarlette Fisher'],
+    'marks': [205]
+})
+
+
+students_data = pd.concat([students_data, new_row], ignore_index=True)
+print(students_data)
+
+
+
+students_data1 = pd.DataFrame({
+    'student_id': ['S1', 'S2', 'S3', 'S4', 'S5'],
+    'name': ['Danniella Fenton', 'Ryder Storey', 'Bryce Jensen', 'Ed Bernal', 'Kwame Morin'],
+    'marks': [200, 210, 190, 222, 199]
+})
+
+students_data2 = pd.DataFrame({
+    'student_id': ['S4', 'S5', 'S6', 'S7', 'S8'],
+    'name': ['Scarlette Fisher', 'Carla Williamson', 'Dante Morse', 'Kaiser William', 'Madeeha Preston'],
+    'marks': [201, 200, 198, 219, 201]
+})
+
+exam_data = pd.DataFrame({
+    'student_id': ['S1', 'S2', 'S3', 'S4', 'S5', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13'],
+    'exam_id': [23, 45, 12, 67, 21, 55, 33, 14, 56, 83, 88, 12]
+})
+
+
+combined_data = pd.concat([students_data1, students_data2], ignore_index=True)
+
+
+final_data = pd.merge(combined_data, exam_data, on='student_id', how='left')
+print(final_data)
+
+
+
+student1 = pd.DataFrame({
+    'student_id': ['S1', 'S2', 'S3', 'S4', 'S5'],
+    'name': ['Danniella Fenton', 'Ryder Storey', 'Bryce Jensen', 'Ed Bernal', 'Kwame Morin'],
+    'marks': [200, 210, 190, 222, 199]
+})
+
+student2 = pd.DataFrame({
+    'student_id': ['S4', 'S5', 'S6', 'S7', 'S8'],
+    'name': ['Scarlette Fisher', 'Carla Williamson', 'Dante Morse', 'Kaiser William', 'Madeeha Preston'],
+    'marks': [201, 200, 198, 219, 201]
+})
+
+
+result3 = pd.merge(student1, student2, on='student_id', how='inner')
+print(result3)
+
+
+
+student3 = pd.DataFrame({
+    'student_id': ['S1', 'S2', 'S3', 'S4', 'S5'],
+    'name': ['Danniella Fenton', 'Ryder Storey', 'Bryce Jensen', 'Ed Bernal', 'Kwame Morin'],
+    'marks': [200, 210, 190, 222, 199]
+})
+
+student4 = pd.DataFrame({
+    'student_id': ['S4', 'S5', 'S6', 'S7', 'S8'],
+    'name': ['Scarlette Fisher', 'Carla Williamson', 'Dante Morse', 'Kaiser William', 'Madeeha Preston'],
+    'marks': [201, 200, 198, 219, 201]
+})
+
+
+result4 = pd.merge(student3, student4, on='student_id', how='inner')
+print(result4)
+
+
+
